df366f4ad2e85db9e69c5114b723be6a1e904399
Bryan Duxbury <bryan.duxbury@gmail.com>
2015-04-24 08:12:08 -0700

    firmware WIP

--- firmware_v1/firmware_v1.ino
+++ firmware_v1/firmware_v1.ino
@@ -1,18 +1,95 @@
+#include "PID_v1.h"
+#include "thermistor.h"
+
+#define HOT_OUT 5
+#define COLD_OUT 10
+#define HOT_IN 0
+#define COLD_IN 1
+#define AMBIENT_IN 2
+
+// (5v / 254 steps) = 19.6 mV / step
+// 5:1 voltage divider = 3.9 mV / step
+// I=V/R, R=0.1, I=10V
+// 39mA / step
+#define MA_PER_STEP 39
+
+double hot_target, hot_current, hot_milliamps;
+double cold_target, cold_current, cold_milliamps;
+
+PID hot_pid(&hot_current, &hot_milliamps, &hot_target, 2, 5, 1, DIRECT);
+// note the cold PID is in reverse, since it is a chiller
+PID cold_pid(&cold_current, &cold_milliamps, &cold_target, 2, 5, 1, REVERSE);
+
+Thermistor hot_therm(HOT_IN, 5000, 9970);
+Thermistor cold_therm(COLD_IN, 5000, 9940);
+Thermistor ambient_therm(AMBIENT_IN, 5000, 10000);
+
 void setup() {
-  pinMode(10, OUTPUT);
-  pinMode(5, OUTPUT);
-  analogWrite(10, 255);
-  analogWrite(5, 255);
+  // for debugging output
+  Serial.begin(115200);
+
+  pinMode(HOT_OUT, OUTPUT);
+  pinMode(COLD_OUT, OUTPUT);
+
+  // start with heater/chiller off
+  analogWrite(0, 255);
+  analogWrite(0, 255);
+
+  hot_target = 41.0;
+  hot_milliamps = 0.0;
+  cold_target = 18.0;
+  cold_milliamps = 0.0;
+
+  // 3A range on hot output
+  hot_pid.SetOutputLimits(0, 3000);
+  hot_pid.SetMode(AUTOMATIC);
+
+  // 6A range on cold output
+  cold_pid.SetOutputLimits(0, 6000);
+  cold_pid.SetMode(AUTOMATIC);
 }
 
 void loop() {
-  // analogWrite(10, 10);
-  // setSinkMilliamps(10, 100);
-//   setSinkMilliamps(5, 3000);
+  updateTemps();
+
+  // update PID computations
+  hot_pid.Compute();
+  cold_pid.Compute();
+
+  // reset output current control
+  setSinkMilliamps(HOT_OUT, hot_milliamps);
+  setSinkMilliamps(COLD_OUT, cold_milliamps);
+
+  // print debugging info
+  Serial.print(ambient_therm.getDegC());
+  Serial.print("\t");
+  Serial.print(hot_target);
+  Serial.print("\t");
+  Serial.print(hot_current);
+  Serial.print("\t");
+  Serial.print(hot_milliamps);
+  Serial.print("\t");
+  Serial.print(cold_target);
+  Serial.print("\t");
+  Serial.print(cold_current);
+  Serial.print("\t");
+  Serial.print(cold_milliamps);
+  Serial.println();
+
+  // no more than 100Hz updates
+  delay(250);
 }
 
-#define VREF 4970
+
+void updateTemps() {
+  hot_current = hot_therm.getDegC();
+  cold_current = cold_therm.getDegC();
+}
+
 void setSinkMilliamps(int pin, uint16_t ma) {
-  int step_size = VREF / 254;
-  analogWrite(pin, ma / 2 / step_size);
+  // Serial.print("Setting pin ");
+  // Serial.print(pin);
+  // Serial.print(" to step ");
+  // Serial.println(ma / MA_PER_STEP);
+  analogWrite(pin, ma / MA_PER_STEP);
 }
---
+++ firmware_v1/thermistor.h
@@ -0,0 +1,48 @@
+#ifndef __THERMISTOR_H__
+#define __THERMISTOR_H__
+
+#include "Arduino.h"
+
+class Thermistor {
+public:
+  Thermistor(uint8_t analogPin, uint16_t refMv, uint16_t refOhms) {
+    pin = analogPin;
+    ref_mv = refMv;
+    ref_ohms = refOhms;
+  }
+
+  float getDegK() {
+    return r2t(v2r(analogRead(pin)));
+  }
+
+  float getDegC() {
+    return getDegK() - 273.15f;
+  }
+
+private:
+  uint8_t pin;
+  uint16_t ref_mv;
+  uint16_t ref_ohms;
+
+  float v2r(int sample) {
+    float mvSample = (sample / 1023.0) * ref_mv;
+    return ref_ohms * (float)ref_mv / (float)mvSample - ref_ohms;
+  }
+
+  // constants for the steinhart-hart extended function
+  #define SH_A1 3.354016E-03
+  #define SH_B1 2.569850E-04
+  #define SH_C1 2.620131E-06
+  #define SH_D1 6.383091E-08
+
+  float r2t(int ohms) {
+    float r = ohms/10000.0;
+    return 1.0f / (
+      SH_A1
+      + SH_B1 * log(r)
+      + SH_C1 * pow(log(r), 2)
+      + SH_D1 * pow(log(r), 3));
+  }
+};
+
+#endif
